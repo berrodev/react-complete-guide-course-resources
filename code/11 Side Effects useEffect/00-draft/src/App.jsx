4aa950fc1d65d7c871f158748fdcfbe7e8840c6a
Francisco Berroeta <fsberroeta@gmail.com>
2024-09-02 19:22:28 -0400

    chore: Refactor App component to use stored places from local storage directly

--- code/11 Side Effects useEffect/00-draft/src/App.jsx
+++ code/11 Side Effects useEffect/00-draft/src/App.jsx
@@ -7,11 +7,12 @@ import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import { sortPlacesByDistance } from './loc.js';
 
+const storedPlaces =
+  JSON.parse(localStorage.getItem('pickedPlaces')).map((id) =>
+    AVAILABLE_PLACES.find((place) => place.id === id)
+  ) || [];
+
 function App() {
-  const storedPlaces =
-    JSON.parse(localStorage.getItem('pickedPlaces')).map((id) =>
-      AVAILABLE_PLACES.find((place) => place.id === id)
-    ) || [];
   const modal = useRef();
   const selectedPlace = useRef();
   const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
